3ad9bf82e652c1294aa231968e9516c02468fec3
awesome-cmyk <0cracka05@gmail.com>
2025-07-02 14:52:04 +0200

    first commit
    07-02-2025

---
+++ js/commands.js
@@ -0,0 +1,83 @@
+var password = "awesome";
+
+whois = [
+    "<br>",
+    "Hey, I'm me!👋",
+    "I'm a 15 year old kid, who likes computers and making things.",
+    "got inspired by \"passwordpassword.online\" from Nikita Kryukov, the creator of milk inside of a bag outisde of a bag, and wanted to",
+    "improve my javascript skills.",
+    "I live in belgium and I can speak English, French, Dutch, and turkish. I have",
+    "experience in javascript, html, css and a little bit of C. I first got",
+    "into coding about 2 years ago when I decided to install arch linux, and yea, it kinda snowballed from there. Right now I am",
+    "using gentoo and as you would guess I support the FOSS movement.",
+    "..",
+    "yea, not much to it.",
+    "I like this terminal website tho.",
+    "<br>"
+];
+
+milk = [
+    "<br>",
+    "milk inside of a bag of milk outisde of a bag of milk inside a bag of ...",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "The age old question, it's really simple actually. You are you.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'I don\'t have socials :D',
+    'I created a github account just for this site !',
+    'so I could host it here',
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Still curating... most projects are offline, or on GitHub.",
+    "I have this site right now, but I am currently working on some C projects.",
+    "I would love to add a filesystem structure to this site but idk, I am kinda focused on C for now.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is the creator of this site?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">secret</span>         Find the password',
+    '<span class="command">projects</span>       View coding projects',
+    '<span class="command">history</span>        View command history',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    '<span class="command">milk</span>           milk inside of a bag of milk outside of ...',
+    "<br>",
+];
+banner = [
+    '<span class="index">2025 aweSome\'sauce, Not A Corporation. All rights reserved.</span>',
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣦⣤⣴⣦⣤⣤⠀⣄⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣴⣶⡟⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠉⢉⠟⠀⠀⠀⠀⠉⠛⣿⠛⠷⡄⣀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⢀⣤⠞⣻⠉⠉⠀⠀⠀⠈⢣⡀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠐⠀⠀⠀⠀⣧⠀⡄⢉⡿⠀⠀⠀",
+    "⠀⠀⠀⣴⠋⠀⠀⡇⠀⠀⠀⠐⠀⠀⢸⡇⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠀⠀⠤⣀⠀⠀⠀⢀⡟⠀⠀⠋⠀⠀⠀⠀",
+    "⠀⠀⠈⠈⠃⠀⠀⠧⠀⣀⠀⠠⠔⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "============================================================",
+    "#####################-WELCOME-AWESOME!-#####################",
+    "============================================================",
+    '<span class="color2">Welcome to MyTerm.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
